910726bf53fc4bb9886363469638eacd9cad8c13
Ben Ross <benjamin.ross@youview.com>
2021-10-28 15:44:15 +0100

    Change test descriptions in index.js

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,13 +4,13 @@ import sinonChai from 'sinon-chai';
 const expect = chai.expect;
 chai.use(sinonChai);
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('index.js', function() {
+  describe('Tests to check everything works correctly', function() {
+    it('should check if 2 equals 2', function() {
       expect(2).to.equal(2);
     });
 
-    it("calls the original function", function () {
+    it("should call the original function", function () {
       var callback = sinon.fake();
 
       callback();
