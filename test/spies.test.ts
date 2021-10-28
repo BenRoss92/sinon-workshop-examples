9995357f5eec992614b40e1aa9091c2c23b3363d
Ben Ross <benjamin.ross@youview.com>
2021-10-28 17:29:34 +0100

    Spy: add callback + ES6 examples

--- test/spies.test.ts
+++ test/spies.test.ts
@@ -6,7 +6,8 @@ chai.use(sinonChai);
 
 describe('spy', () => {
     describe("#called", function () {
-        it("should have been called once", function () {
+        it("Plain object: should have been called once with exact argument", function () {
+            // Can spy on a function inside of an object
             const object = { 
                 method(arg: number): void {
                     // no-op
@@ -21,22 +22,41 @@ describe('spy', () => {
             expect(spy).to.have.been.calledOnce;
             expect(spy).to.have.been.calledOnceWithExactly(1);
         });
+
+        it('Callback function: should have been called once', () => {
+            function myFunction(condition: boolean, callback: () => void){
+                if(condition){
+                  callback();
+                }
+              }
+              
+            const callback = sinon.spy();
+        
+            myFunction(true, callback);
+        
+            expect(callback).to.have.been.calledOnce;
+        });
     });
 
     describe('#getCall and #getCalls', () => {
-        it('should ', () => {
-            const object = { 
-                method(arg: number): void {
-                    // no-op
-                } 
-            };
+        it('should let us know what arguments were received in each call', () => {
 
-            const spy = sinon.spy(object, 'method');
+            // Can spy on an ES6 instance method
+            class Roulette {
+                public betOnNumber (number: number) {
+                    // do something ...
+                }
+            }
 
-            object.method(6);
-            object.method(8);
-            object.method(10);
+            const roulette = new Roulette();
 
+            const spy = sinon.spy(roulette, 'betOnNumber');
+
+            roulette.betOnNumber(6);
+            roulette.betOnNumber(8);
+            roulette.betOnNumber(10);
+            
+            // One way
             const firstCall = spy.getCall(0);
             const firstCallFirstArg = firstCall.args[0];
             
@@ -45,13 +65,13 @@ describe('spy', () => {
             
             const thirdCall = spy.getCall(2);
             const thirdCallFirstArg = thirdCall.args[0];
-
-            const calls = spy.getCalls();
-            console.log(calls);
             
             expect(firstCallFirstArg).to.equal(6);
             expect(secondCallFirstArg).to.equal(8);
             expect(thirdCallFirstArg).to.equal(10);
+
+            // Another way
+            const calls = spy.getCalls();
 
             expect(calls[0].args[0]).to.equal(6);
             expect(calls[1].args[0]).to.equal(8);
